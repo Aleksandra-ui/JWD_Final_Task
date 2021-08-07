599c826dbfa6acbbf161549881a45b7a29210bff
Sanya <alexandra19bye@gmail.com>
2021-08-07 16:33:01 +0300

    localization servlet

---
+++ src/com/epam/jwd/apotheca/controller/LocaleServlet.java
@@ -0,0 +1,34 @@
+package com.epam.jwd.apotheca.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LocaleServlet extends HttpServlet {
+
+	/****/
+	private static final long serialVersionUID = 6151651810369784194L;
+	
+	@Override
+	public void destroy() {
+		super.destroy();
+	}
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {	
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+}
