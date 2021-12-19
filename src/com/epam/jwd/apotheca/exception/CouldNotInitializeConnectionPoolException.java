ca40fbbd4630bdd6d9a4994577a9b91be1ba201e
Sanya <alexandra19bye@gmail.com>
2021-12-19 16:13:09 +0300

    exceptions cleaning

--- src/com/epam/jwd/apotheca/exception/CouldNotInitializeConnectionPoolException.java
+++ src/com/epam/jwd/apotheca/exception/CouldNotInitializeConnectionPoolException.java
@@ -2,12 +2,10 @@ package com.epam.jwd.apotheca.exception;
 
 public class CouldNotInitializeConnectionPoolException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -6270552880462489272L;
 
 	public CouldNotInitializeConnectionPoolException(String message, Throwable cause) {
 		super(message, cause);
 	}
+	
 }
